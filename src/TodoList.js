2c01e180ccca20c7f01c388c5508fb35124e49ba
mamiline6 <ohmamirineoo6@gmail.com>
2023-10-19 11:35:52 +0900

    完成

---
+++ src/TodoList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Todo from './Todo';
+
+// 関数コンポーネント
+const TodoList = ({ todos, toggleTodo }) => {
+    return todos.map((todo) => (
+        <Todo todo={todo} key={todo.id} toggleTodo={toggleTodo} />
+    ));
+};
+
+// どのファイルでも使えるようにする
+export default TodoList;
